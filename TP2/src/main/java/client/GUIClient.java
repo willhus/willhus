14a3cf182e7a01c73733646697c0412b0a06bb2a
willhus <117056232+willhus@users.noreply.github.com>
2023-04-12 19:12:26 -0400

    fix: fix some GUI bugs
    Co-authored-by: Julien Hu <hujulien123@gmail.com>

--- TP2/src/main/java/client/GUIClient.java
+++ TP2/src/main/java/client/GUIClient.java
@@ -6,17 +6,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class GUIClient extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("client.fxml"));
-        Parent root = loader.load();
-        Scene scene = new Scene(root);
-        
-        primaryStage.setTitle("Course Registration");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("src/main/resources/main_view.fxml"));
+            Parent root = fxmlLoader.load();
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
